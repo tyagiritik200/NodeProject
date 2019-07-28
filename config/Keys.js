2994c680f2f1d77865e354498415a81a550874fe
tyagiritik200 <53155164+tyagiritik200@users.noreply.github.com>
2019-07-28 14:08:20 +0530

    Update Keys.js

--- config/Keys.js
+++ config/Keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MongoURI: "mongodb://localhost:27017/ritik"
+    MongoURI:"mongodb+srv://Ritik:Ritik@123@ritikcluster-wlgl1.mongodb.net/test?retryWrites=true&w=majority"
+    //MongoURI: "mongodb://localhost:27017/ritik"
 }
